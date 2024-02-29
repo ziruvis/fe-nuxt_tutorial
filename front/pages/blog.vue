75c30b9e266993aaa99f18e0d5c663b905bbb8c0
ziruvis <ziruvis3104@gmail.com>
2024-02-29 22:49:03 +0900

    first commit

---
+++ front/pages/blog.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div v-for="singleData in data" :key="singleData.id">
+            {{ singleData.title }}
+            <hr>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const { data } = await useAsyncData("blogQuery",() => queryContent("/blog").find())
+</script>
+
+<style>
+</style>
---
+++ front/pages/contact.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        Contactページ
+    </h1>
+</template>
